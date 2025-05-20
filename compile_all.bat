5837cb54b8fa7d5e9ea212220877eabea8bfafed
ian <topduckofficial@outlook.com>
2025-05-20 17:20:34 -0500

    Add move function to the compile commands

--- compile_all.bat
+++ compile_all.bat
@@ -1,1 +1,6 @@
-multify -c -f  MadRayneTTCCPack.mf phase_3 phase_3.5 phase_4 phase_5 phase_5.5 phase_6 phase_7 phase_8 phase_9 phase_10 phase_11 phase_12 phase_13 phase_14
+set "multifile=MadRayneTTCCPack.mf"
+set "destination=%localappdata%\Corporate Clash\resources\contentpacks\"
+
+multify -c -f  %multifile% phase_3 phase_3.5 phase_4 phase_5 phase_5.5 phase_6 phase_7 phase_8 phase_9 phase_10 phase_11 phase_12 phase_13 phase_14
+
+move "%multifile%" "%destination%"
--- compile_single.bat
+++ compile_single.bat
@@ -1,6 +1,20 @@
 @echo off
-set /p id=Select a phase file to compile: 
+set /p id=Select a phase file to compile (You can also compile subfolder): 
+
 echo Creating...
 @echo off
-multify -c -f phase_%id:/=_%.mf phase_%id%
+set "filename=phase_%id:/=_%.mf"
+multify -c -f %filename% phase_%id%
 echo Done.
+
+set "directory=%localappdata%\Corporate Clash\resources\contentpacks\"
+echo File created: "%filename%"
+echo Destination: "%directory%"
+
+set /p MOVE_CHOICE= Move this file to the contentpacks folder? (Y/any key for N)
+
+If /I "%MOVE_CHOICE%" == "y" (
+move "%filename%" "%directory%" ) else ( 
+echo Have a nice day
+@echo off
+@timeout /t 3 >nul )
